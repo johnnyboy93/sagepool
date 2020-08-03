9ee4c75fa72e903c9e07ffc3320e8a6348c0b68f
johnnyboy93 <johnconstantinedewitt@gmail.com>
2020-08-03 15:06:48 +0800

    fixed footer empty href

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -45,7 +45,7 @@ const Footer = ({
           </div>
           <div className="footer-bottom space-between text-xxs invert-order-desktop">
             <FooterNav />
-            <div className="footer-copyright">Made by the <a href="">Sage</a> master. All right reserved</div>
+            <div className="footer-copyright">Made by the <a href="www.google.com">Sage</a> master. All right reserved</div>
           </div>
         </div>
       </div>
